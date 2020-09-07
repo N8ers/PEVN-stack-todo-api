3646becfacfca6bfb1718dbb28490e4628481519
nathan sheryak <nathanraysheryak@gmail.com>
2020-09-07 12:44:18 -0400

    non jwt - connection checker

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/welcome", async (req, res) => {
+  res.status(200).json({ message: "Welcome, Nathan" });
+});
+
 module.exports = router;
