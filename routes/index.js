ca38306b6f7ebd0c16921ebf0ae71557c12cdaec
nathan sheryak <nathanraysheryak@gmail.com>
2020-09-12 08:24:04 -0400

    new route to test db connection

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../database/db");
 
 router.get("/", async (req, res) => {
   res.status(200).json({ message: "Welcome, to the PEVN fullstack api!" });
@@ -7,6 +8,17 @@ router.get("/", async (req, res) => {
 
 router.get("/welcome", async (req, res) => {
   res.status(200).json({ message: "Welcome, Nathan" });
+});
+
+router.get("/db-connection-status", async (req, res) => {
+  
+  db.connect((err) => {
+    if (err) {
+      res.send("CONNECTION ERROR: ", err);
+    } else {
+      res.send("CONNECTED!");
+    }
+  });
 });
 
 module.exports = router;
