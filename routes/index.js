ad80d2b536862d899fd3ea080d7c07d6433c916e
nathan sheryak <nathanraysheryak@gmail.com>
2020-09-12 06:59:39 -0400

    get message for default route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  res.status(200).json({ message: "Welcome, to the PEVN fullstack api!" });
+});
+
 router.get("/welcome", async (req, res) => {
   res.status(200).json({ message: "Welcome, Nathan" });
 });
